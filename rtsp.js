6fec263767b9b78a7e2e9034f9e148be19ffff8c
Jorge Anchondo Hernandez <jorge.anchondo@policiachihuahua.gob.mx>
2022-03-14 11:17:16 -0700

    skskssdas

--- rtsp.js
+++ rtsp.js
@@ -56,7 +56,7 @@ app.get(
   <script src='${scriptUrl}'></script>
   <script>
     loadPlayer({
-      url: 'ws://10.218.53.45:8080/api/streams?url=${req.query.url}',
+      url: 'ws://https://rtsp-deploy.herokuapp.com/api/streams?url=${req.query.url}',
       canvas: document.getElementById('canvas')
     });
   </script>
